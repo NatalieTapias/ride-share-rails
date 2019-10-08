e32dfbe0327aa07fdfabf6e37665f0edec77d7b7
Rinostar <codingrinostar@gmail.com>
2019-10-08 15:04:41 -0700

    added new, create, and show actions to Driver

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -7,4 +7,26 @@ class DriversController < ApplicationController
     driver_id = params[:id]
     @driver = Driver.find_by(id: driver_id)
   end
+
+  def new
+    @driver = Driver.new
+  end 
+
+  def create
+    @driver = Driver.new(driver_params)
+    if @driver.save
+      redirect_to driver_path(@driver)
+      return
+    else
+      render :new
+      return
+    end
+  end 
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
+
 end
