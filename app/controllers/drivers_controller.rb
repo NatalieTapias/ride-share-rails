11d9dfa856fe69aa3089a71c0530089b876f59fd
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-08 15:50:07 -0700

    Edit Index and Show actions for DriversController

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -7,11 +7,11 @@ class DriversController < ApplicationController
     driver_id = params[:id]
     @driver = Driver.find_by(id: driver_id)
   end
-
+  
   def new
     @driver = Driver.new
   end 
-
+  
   def create
     @driver = Driver.new(driver_params)
     if @driver.save
@@ -22,11 +22,47 @@ class DriversController < ApplicationController
       return
     end
   end 
-
+  
+  def edit
+    @driver = Driver.find_by(id: params[:id])
+    
+    if @driver.nil?
+      flash[:error] = "Could not find driver ID #{ @driver.id }"
+      redirect_to drivers_path
+      return
+    end 
+  end
+  
+  def update
+    @driver = Driver.find_by(id: params[:id])
+    
+    if @driver.update(driver_params)
+      redirect_to drivers_path(@driver)
+      return
+    else 
+      render :edit
+      return
+    end
+  end
+  
+  def destroy
+    @driver = Driver.find_by(id: params[:id])
+    
+    if @driver.nil?
+      flash[:error] = "Could not find driver ID #{ @driver.id }"
+      redirect_to drivers_path
+      return
+    end 
+    
+    @driver.destroy
+    redirect_to drivers_path
+    return
+  end
+  
   private
-
+  
   def driver_params
     return params.require(:driver).permit(:name, :vin)
   end
-
+  
 end
