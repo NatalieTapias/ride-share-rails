4f0e3e5f2d9c2fbddf3935f277a412d99e334e2f
Rinostar <codingrinostar@gmail.com>
2019-10-08 15:27:24 -0700

    added new, show, create actions to Trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,7 +2,31 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
   end
-  
+   
   def show
+    trip_id = params[:id]
+    @trip = Trip.find_by(id: trip_id)
   end
+
+  def new
+    @trip = Trip.new
+  end 
+
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      redirect_to trip_path(@trip)
+      return
+    else
+      render :new
+      return
+    end
+  end 
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(:date, :rating, :cost, :passenger_id, :driver_id)
+  end
+
 end
