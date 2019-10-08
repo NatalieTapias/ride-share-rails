ff53f740b4c3a450e6182bdd2c6da3740c618a5b
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-08 15:55:10 -0700

    Added Passenger#Edit and Passenger#Delete actions.

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -7,11 +7,11 @@ class PassengersController < ApplicationController
     passenger_id = params[:id]
     @passenger = Passenger.find_by(id: passenger_id)
   end
-
+  
   def new
     @passenger = Passenger.new
   end 
-
+  
   def create
     @passenger = Passenger.new(passenger_params)
     if @passenger.save
@@ -22,11 +22,47 @@ class PassengersController < ApplicationController
       return
     end
   end 
-
+  
+  def edit
+    @passenger = Passenger.find_by(id: params[:id])
+    
+    if @passenger.nil?
+      flash[:error] = "Could not find passenger ID #{ @passenger.id }"
+      redirect_to passengers_path
+      return
+    end 
+  end
+  
+  def update
+    @passenger = Passenger.find_by(id: params[:id])
+    
+    if @passenger.update(passenger_params)
+      redirect_to passengers_path(@passenger)
+      return
+    else 
+      render :edit
+      return
+    end
+  end
+  
+  def destroy
+    @passenger = Passenger.find_by(id: params[:id])
+    
+    if @passenger.nil?
+      flash[:error] = "Could not find passenger ID #{ @passenger.id }"
+      redirect_to passengers_path
+      return
+    end 
+    
+    @passenger.destroy
+    redirect_to passengers_path
+    return
+  end
+  
   private
-
+  
   def passenger_params
     return params.require(:passenger).permit(:name, :phone_num)
   end
-
+  
 end
