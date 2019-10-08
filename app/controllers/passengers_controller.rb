7630e1164c2ffded6d7eea713ff1ab099a57b806
Rinostar <codingrinostar@gmail.com>
2019-10-08 15:19:51 -0700

    added new, show, create actions to Passenger

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -4,5 +4,29 @@ class PassengersController < ApplicationController
   end
   
   def show
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
   end
+
+  def new
+    @passenger = Passenger.new
+  end 
+
+  def create
+    @passenger = Passenger.new(passenger_params)
+    if @passenger.save
+      redirect_to passenger_path(@passenger)
+      return
+    else
+      render :new
+      return
+    end
+  end 
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
+
 end
