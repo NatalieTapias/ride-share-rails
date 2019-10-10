d3195fdd430983fe27146004a1ba7f9c5c0f4904
Rinostar <codingrinostar@gmail.com>
2019-10-10 16:27:17 -0700

    added total_earning & average_rating to Driver (model & view)

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,32 @@ class Driver < ApplicationRecord
 
   validates :name, presence: true
   validates :vin, presence: true
+
+  def total_earning
+    sum = 0
+    self.trips.each do |trip|
+      if trip.cost != nil
+        sum += (trip.cost - 1.65) * 0.8
+      end
+    end
+    return sum
+  end
+
+  def average_rating
+    sum = 0
+    count = 0
+    self.trips.each do |trip|
+      if trip.rating != nil
+        sum += (trip.rating)
+        count += 1
+      end
+    end
+    
+    
+    if count == 0
+      return "There is no rating info avaliable"
+    else
+      return (sum / count)
+    end
+  end
 end
