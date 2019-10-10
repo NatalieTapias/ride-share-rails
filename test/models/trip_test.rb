f76f9a188c89b49b80fa454c620429d803b96da7
Rinostar <codingrinostar@gmail.com>
2019-10-09 17:57:57 -0700

    updated model tests

--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -1,16 +1,29 @@
 require "../test_helper"
 
 describe Trip do
+  let (:new_driver) {
+    Driver.new(name: "Kari", vin: "123", active: true)
+  }
+  
+
+  let (:new_passenger) {
+    Passenger.new(name: "Kari", phone_num: "111-111-1211")
+  }
+  
+
   let (:new_trip) {
     Trip.new(
       date: Date.today,
-      rating: , 
-      cost: nil, 
-      passenger_id: 1,
-      driver_id: 1
+      cost: 10, 
+      passenger_id: new_passenger.id,
+      driver_id: new_driver.id
     )
   }
+
   it "can be instantiated" do
+    #Arrange
+    new_driver.save
+    new_passenger.save
     # Assert
     expect(new_trip.valid?).must_equal true
   end
@@ -19,7 +32,7 @@ describe Trip do
     # Arrange
     new_trip.save
     trip = Trip.first
-    [:date, :rating, :cost, :passenger_id, :driver_id].each do |field|
+    [:date, :passenger_id, :driver_id, :cost, :rating].each do |field|
 
       # Assert
       expect(trip).must_respond_to field
